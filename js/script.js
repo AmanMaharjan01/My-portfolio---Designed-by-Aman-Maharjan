8e37adff42bf339ef3b6a103083380c41924bfe2
Amanmaharjan01 <maharjan06@gmail.com>
2022-02-02 21:26:21 +0545

    almost fina\l

--- js/script.js
+++ js/script.js
@@ -1,34 +1,50 @@
+document.getElementById("home").setAttribute("class", "active");
 
-document.getElementById('home').setAttribute("class","active");
-
-function about()
-{
-	document.getElementById('aboutme').setAttribute("class","active");
-	document.getElementById('home').removeAttribute("class");
-	document.getElementById('myskills').removeAttribute("class");
-	document.getElementById('contactme').removeAttribute("class");
+function about() {
+  document.getElementById("aboutme").setAttribute("class", "active");
+  document.getElementById("home").removeAttribute("class");
+  document.getElementById("myskills").removeAttribute("class");
+  document.getElementById("contactme").removeAttribute("class");
 }
 
-function home()
-{
-	document.getElementById('aboutme').removeAttribute("class");
-	document.getElementById('home').setAttribute("class","active");
-	document.getElementById('myskills').removeAttribute("class");
-	document.getElementById('contactme').removeAttribute("class");
+function home() {
+  document.getElementById("aboutme").removeAttribute("class");
+  document.getElementById("home").setAttribute("class", "active");
+  document.getElementById("myskills").removeAttribute("class");
+  document.getElementById("contactme").removeAttribute("class");
 }
 
-function skills()
-{
-	document.getElementById('aboutme').removeAttribute("class");
-	document.getElementById('home').removeAttribute("class");
-	document.getElementById('myskills').setAttribute("class","active");
-	document.getElementById('contactme').removeAttribute("class");
+function skills() {
+  document.getElementById("aboutme").removeAttribute("class");
+  document.getElementById("home").removeAttribute("class");
+  document.getElementById("myskills").setAttribute("class", "active");
+  document.getElementById("contactme").removeAttribute("class");
 }
 
-function contact()
-{
-	document.getElementById('aboutme').removeAttribute("class");
-	document.getElementById('home').removeAttribute("class");
-	document.getElementById('myskills').removeAttribute("class");
-	document.getElementById('contactme').setAttribute("class","active");
+function contact() {
+  document.getElementById("aboutme").removeAttribute("class");
+  document.getElementById("home").removeAttribute("class");
+  document.getElementById("myskills").removeAttribute("class");
+  document.getElementById("contactme").setAttribute("class", "active");
 }
+
+function videoUrl(video) {
+  document.getElementById("slider").src = video;
+}
+
+var app = document.getElementById("about-detail");
+
+var typewriter = new Typewriter(app, {
+  loop: true,
+});
+
+typewriter
+  .typeString("Web Developer")
+  .pauseFor(1800)
+  .deleteAll()
+  .typeString("Web Designer")
+  .pauseFor(1800)
+  .deleteAll()
+  .typeString("Technology enthusiast")
+  .pauseFor(1800)
+  .start();
